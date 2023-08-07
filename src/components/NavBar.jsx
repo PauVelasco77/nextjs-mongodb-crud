e3ad7a0013e2adde7b7a77d38f0bc31c73180040
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-07 19:44:46 +0200

    create NAvBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-gray-950 py-5 mb-2">
+      <div className="container flex justify-between px-10 md:px-0  mx-auto text-white">
+        <Link href="/">
+          <h1 className="text-2xl font-bold"> Next Mongo App </h1>
+        </Link>
+        <ul className="flex gap-x-4">
+          <li>
+            <Link href="/tasks/new">New task</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
