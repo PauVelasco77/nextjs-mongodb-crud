48646893ec501afe722f0ce5889c7d3eb1e6fb73
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-07 15:23:40 +0200

    call connectDB to ping endpoint

--- src/app/api/ping/route.js
+++ src/app/api/ping/route.js
@@ -1,5 +1,7 @@
+import {connectDB} from "@/utils/mongoose";
 import {NextResponse} from "next/server";
 
 export function GET() {
+  connectDB();
   return NextResponse.json({message: "Hello World!"});
 }
