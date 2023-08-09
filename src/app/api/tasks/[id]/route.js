610e5faf588d9897a39acb63a909cc23f25d34a9
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-09 18:40:12 +0200

    adfsadfs

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,66 +1,66 @@
 import NextCors from 'nextjs-cors'
 import { NextResponse } from 'next/server'
-// import { connectDB } from '@/utils/mongoose'
+import { connectDB } from '@/utils/mongoose'
 import Task from '@/models/Task'
 
+// export async function GET (req, res) {
+//   await NextCors(req, res, {
+//     // Options
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//     origin: '*',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   })
+//   console.log('GET')
+//   return NextResponse.json({
+//     // task
+//     id: '1',
+//     title: 'Tarea 1',
+//     description: 'Descripcion de la tarea 1'
+//   })
+//   // return new Response('Hello, Next.js!', {
+//   //   status: 200,
+//   //   headers: {
+//   //     'Access-Control-Allow-Origin': '*',
+//   //     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   //     'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+//   //   }
+//   // })
+// }
+
 export async function GET (req, res) {
-  await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  })
-  console.log('GET')
-  return NextResponse.json({
-    // task
-    id: '1',
-    title: 'Tarea 1',
-    description: 'Descripcion de la tarea 1'
-  })
-  // return new Response('Hello, Next.js!', {
-  //   status: 200,
-  //   headers: {
-  //     'Access-Control-Allow-Origin': '*',
-  //     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  //     'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-  //   }
-  // })
+  await connectDB()
+  // console.log('GET', res.params.id)
+  const validId = res.params.id
+
+  try {
+    const task = await Task.findById(validId)
+
+    if (!task) {
+      console.log('Tarea no encontrada')
+      return NextResponse.json(
+        {
+          error: true,
+          message: 'Task not found'
+        },
+        {
+          status: 404
+        }
+      )
+    }
+
+    return NextResponse.json(task)
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: true,
+        message: error.message
+      },
+      {
+        status: 400
+      }
+    )
+  }
 }
-
-// export async function GET (req, { params }) {
-// await connectDB()
-// const validId = params.id
-
-// try {
-//   const task = await Task.findById(validId)
-
-//   if (!task) {
-//     console.log('Tarea no encontrada')
-//     return NextResponse.json(
-//       {
-//         error: true,
-//         message: 'Task not found'
-//       },
-//       {
-//         status: 404
-//       }
-//     )
-//   }
-
-//   return NextResponse.json(task)
-// } catch (error) {
-//   return NextResponse.json(
-//     {
-//       error: true,
-//       message: error.message
-//     },
-//     {
-//       status: 400
-//     }
-//   )
-// }
-
-// }
 
 export async function PUT (req, { params }) {
   try {
