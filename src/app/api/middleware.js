2056ca053c7bec6389fcd2fc05406f8fbe7b030a
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-10 17:15:25 +0200

    add middleware to manage cors

---
+++ src/app/api/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+
+export async function middleware (request) {
+  const response = NextResponse.next()
+
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    response.headers.append('Access-Control-Allow-Origin', '*')
+  }
+
+  return response
+}
