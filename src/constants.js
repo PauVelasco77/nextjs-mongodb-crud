0ec757a1bafb7b4cb4d74a981f0d13aa829e765c
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-13 20:33:43 +0200

    remove console.logs

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,2 @@
 const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ? 'http' : 'https'
-console.log('protocol', protocol)
 export const API_URL = `${protocol}://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
