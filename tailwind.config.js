3b9e8e8468291b91848f3776c74e33d709cd8f72
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2023-08-20 17:42:23 +0200

    add tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000',
+        white: '#fff',
+        gray: {
+          50: '#f9fafb',
+          100: '#f4f5f7',
+          200: '#e5e7eb',
+          300: '#d2d6dc',
+          400: '#333333',
+          500: '#262626',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#252f3f',
+          900: '#161e2e'
+        },
+        secondary1: 'var(--secondary1)',
+        secondary2: 'var(--secondary2)'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
