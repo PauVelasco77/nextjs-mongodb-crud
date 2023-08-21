16d6857a12f2ade8fb3588a7d2e3835cf6087211
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-21 15:32:05 +0200

    add handleChangeStatus action on useHome custom hook

--- src/hooks/useHomePage.js
+++ src/hooks/useHomePage.js
@@ -3,7 +3,7 @@ import useApi from './useApi'
 
 export default function useHomePage () {
   const [tasks, setTasks] = useState([])
-  const { getAllTasks, deleteTask } = useApi()
+  const { getAllTasks, deleteTask, updateTask } = useApi()
 
   const handleDeleteTask = (id) => {
     deleteTask(id).then(() => {
@@ -11,9 +11,17 @@ export default function useHomePage () {
     }).catch((error) => { throw new Error(error) })
   }
 
+  const handleChangeStatus = (task) => {
+    const newTask = { ...task, completed: !task.completed }
+
+    updateTask(newTask).then(() => {
+      setTasks(tasks.map((task) => task.id === newTask.id ? newTask : task))
+    }).catch((error) => { throw new Error(error) })
+  }
+
   useEffect(() => {
     getAllTasks().then((data) => setTasks(data)).catch((error) => { throw new Error(error) })
   }, [getAllTasks])
 
-  return { tasks, handleDeleteTask }
+  return { tasks, handleDeleteTask, handleChangeStatus }
 }
