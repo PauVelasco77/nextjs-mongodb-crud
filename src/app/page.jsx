32f4de7f68cc1b49e43966066fb2135369993dd2
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-21 15:32:47 +0200

    instantiate change complete status action on TaskCard & page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,14 +10,14 @@ const inter = Inter({
 })
 
 export default function HomePage () {
-  const { tasks, handleDeleteTask } = useHomePage()
+  const { tasks, handleDeleteTask, handleChangeStatus } = useHomePage()
 
   return (
     <div>
       <h1>Home Page</h1>
       <div className={`grid grid-rows-none gap-4 place-items-center ${inter.className}`}>
         {tasks.map((task) =>
-          <TaskCard task={task} key={task.id} actionOnDelete={handleDeleteTask} />
+          <TaskCard task={task} key={task.id} actionOnDelete={handleDeleteTask} actionOnComplete={handleChangeStatus} />
         )}
       </div>
     </div>
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,7 +1,7 @@
 import DeleteButton from './DeleteButton'
 import CheckButton from './CheckButton/CheckButton'
 
-export default function TaskCard ({ task, actionOnDelete }) {
+export default function TaskCard ({ task, actionOnDelete, actionOnComplete }) {
   function capitalizeFirstLetter (str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
@@ -11,15 +11,12 @@ export default function TaskCard ({ task, actionOnDelete }) {
   return (
     <div className=' bg-gray-500 p-2 rounded-lg transition-colors hover:border-secondary2 flex justify-between items-center gap-5 break-keep w-3/5 border border-gray-400'>
       <div className='flex items-center gap-4'>
-        <CheckButton checked={task.completed} actionOnClick={() => {}} />
+        <CheckButton checked={task.completed} actionOnClick={async () => await actionOnComplete(task)} />
         <div className='flex gap-1'>
           <h3 className={`text-sm whitespace-nowrap font-normal ${task.completed ? 'line-through text-gray-300' : 'text-gray-100'}`}>{title}</h3>
         </div>
       </div>
-      <DeleteButton actionOnClick={async () => {
-        await actionOnDelete(task.id)
-      }}
-      />
+      <DeleteButton actionOnClick={async () => await actionOnDelete(task.id)} />
     </div>
   )
 }
