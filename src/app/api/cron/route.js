a4268dff219914fbeb7a84013abf0a168e52b150
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-24 16:58:59 +0200

    create initial task on cron function

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,5 +3,12 @@ import { connectDB } from '@/utils/mongoose'
 
 export async function GET () {
   await connectDB()
+
   await Task.deleteMany({})
+
+  const newTask = new Task({
+    title: 'Learn React'
+  })
+
+  await newTask.save()
 }
