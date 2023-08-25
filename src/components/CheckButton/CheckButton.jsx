657a0cf2f5d651c1eab91d66c0f6098699005932
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-25 12:27:42 +0200

    create Loading Task Card component

--- src/components/CheckButton/CheckButton.jsx
+++ src/components/CheckButton/CheckButton.jsx
@@ -5,10 +5,10 @@ import styles from './CheckButton.module.css'
 
 export default function CheckButton ({ checked, actionOnClick }) {
   return (
-    <button onClick={actionOnClick}>
+    <button onClick={actionOnClick} className='flex justify-center items-center'>
       {checked
         ? <CheckCircleIcon className='h-6 w-6 text-secondary1' />
-        : <div className={styles.iconContainer}>
+        : <div className={`${styles.iconContainer} h-6 w-6 flex justify-center items-center`}>
           <PanoramaFishEyeIcon className='h-6 w-6 text-secondary2' />
           <CheckCircleIcon className='h-6 w-6 text-secondary1 transition-opacity opacity-50 active:opacity-100' />
           </div>}
