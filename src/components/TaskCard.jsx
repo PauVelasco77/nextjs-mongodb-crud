1db06ad76ad645efebaae86975472a7b26511666
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-25 12:48:22 +0200

    improve TaskCard component reading

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,8 +2,16 @@ import DeleteButton from './DeleteButton'
 import CheckButton from './CheckButton/CheckButton'
 
 export default function TaskCard ({ task, actionOnDelete, actionOnComplete }) {
-  function capitalizeFirstLetter (str) {
+  const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  const handleComplete = async () => {
+    await actionOnComplete(task)
+  }
+
+  const handleDelete = async () => {
+    await actionOnDelete(task.id)
   }
 
   const title = capitalizeFirstLetter(task.title)
@@ -11,12 +19,12 @@ export default function TaskCard ({ task, actionOnDelete, actionOnComplete }) {
   return (
     <div className=' bg-gray-500 p-2 rounded-lg transition-colors hover:border-secondary2 flex justify-between items-center gap-5 break-keep w-full border-2 border-gray-400'>
       <div className='flex items-center gap-4'>
-        <CheckButton checked={task.complete} actionOnClick={async () => await actionOnComplete(task)} />
+        <CheckButton checked={task.complete} actionOnClick={handleComplete} />
         <div className='flex gap-1'>
           <h3 className={`text-sm whitespace-nowrap font-normal ${task.complete ? 'line-through text-gray-300' : 'text-gray-100'}`}>{title}</h3>
         </div>
       </div>
-      <DeleteButton actionOnClick={async () => await actionOnDelete(task.id)} />
+      <DeleteButton actionOnClick={handleDelete} />
     </div>
   )
 }
