1ba8da8120dc070a45d883c05e9ae6989cda0d69
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-08-26 18:41:47 +0200

    use same function to update tasks and complete, and add debounce to update title

--- src/components/CheckButton/CheckButton.jsx
+++ src/components/CheckButton/CheckButton.jsx
@@ -2,16 +2,34 @@
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import PanoramaFishEyeIcon from '@mui/icons-material/PanoramaFishEye'
 import styles from './CheckButton.module.css'
+import { useState } from 'react'
+import { TASK } from '@/constants'
 
-export default function CheckButton ({ checked, actionOnClick }) {
+export default function CheckButton ({ complete, actionOnClick }) {
+  const [checked, setChecked] = useState(complete)
+
   return (
-    <button onClick={actionOnClick} className='flex justify-center items-center'>
+    <>
+    <label htmlFor={TASK.complete} className='cursor-pointer'>
+        <input
+          type='checkbox'
+          id={TASK.complete}
+          checked={checked}
+          onClick={(e) => {
+            e.preventDefault()
+            setChecked(e.target.checked)
+            actionOnClick(e)
+          }}
+          name={TASK.complete}
+          className='hidden'
+        />
       {checked
         ? <CheckCircleIcon className='h-6 w-6 text-secondary1' />
         : <div className={`${styles.iconContainer} h-6 w-6 flex justify-center items-center`}>
           <PanoramaFishEyeIcon className='h-6 w-6 text-secondary2' />
           <CheckCircleIcon className='h-6 w-6 text-secondary1 transition-opacity opacity-50 active:opacity-100' />
           </div>}
-    </button>
+    </label>
+    </>
   )
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,27 +1,42 @@
 import DeleteButton from './DeleteButton'
 import CheckButton from './CheckButton/CheckButton'
+import { useCallback, useState } from 'react'
+import { capitalizeFirstLetter } from '@/utils/capitalize'
+import debounce from 'just-debounce-it'
+import { TASK } from '@/constants'
 
-export default function TaskCard ({ task, actionOnDelete, actionOnComplete }) {
-  const capitalizeFirstLetter = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-  }
-
-  const handleComplete = async () => {
-    await actionOnComplete(task)
-  }
+export default function TaskCard ({ taskData, actionOnDelete, actionOnComplete, actionOnUpdate }) {
+  const [task, setTask] = useState({
+    ...taskData,
+    title: capitalizeFirstLetter(taskData.title)
+  })
 
   const handleDelete = async () => {
     await actionOnDelete(task.id)
   }
 
-  const title = capitalizeFirstLetter(task.title)
+  const debouncedUpdateTask = useCallback(debounce(task => {
+    actionOnUpdate(task)
+  }, 300), [])
+
+  const handleComplete = (e) => {
+    const newTask = { ...task, [e.target.name]: e.target.checked }
+    setTask(newTask)
+    actionOnUpdate(newTask)
+  }
+
+  const handleUpdate = (e) => {
+    const newTask = { ...task, [e.target.name]: e.target.value }
+    setTask(newTask)
+    debouncedUpdateTask(newTask)
+  }
 
   return (
     <div className=' bg-gray-500 p-2 rounded-lg transition-colors hover:border-secondary2 flex justify-between items-center gap-5 break-keep w-full border-2 border-gray-400'>
       <div className='flex items-center gap-4'>
-        <CheckButton checked={task.complete} actionOnClick={handleComplete} />
+        <CheckButton complete={task.complete} actionOnClick={handleComplete} />
         <div className='flex gap-1'>
-          <h3 className={`text-sm whitespace-nowrap font-normal ${task.complete ? 'line-through text-gray-300' : 'text-gray-100'}`}>{title}</h3>
+          <input type='text' onChange={handleUpdate} name={TASK.title} value={task.title} className={`text-sm whitespace-nowrap font-normal bg-transparent outline-none ${task.complete ? 'line-through text-gray-300' : 'text-gray-100'}`} />
         </div>
       </div>
       <DeleteButton actionOnClick={handleDelete} />
