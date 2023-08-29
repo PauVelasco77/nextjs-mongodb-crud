5c1782e9d49ee1b46b6f1be11e37d53d27bb24bf
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2023-08-29 23:20:35 +0200

    sort tasks on get & create tasks

--- src/hooks/useHomePage.js
+++ src/hooks/useHomePage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import useApi from './useApi'
 import { mapTaskFromApi } from '@/utils/mapApiTasks'
+import { sortTasks } from '@/utils/sortTasksByComplete'
 
 export default function useHomePage () {
   const [tasks, setTasks] = useState(null)
@@ -12,7 +13,11 @@ export default function useHomePage () {
   const { getAllTasks, deleteTask, updateTask, createTask } = useApi()
 
   useEffect(() => {
-    getAllTasks().then((data) => setTasks(data)).catch((error) => { throw new Error(error) })
+    getAllTasks()
+      .then((data) =>
+        setTasks(sortTasks(data))
+      )
+      .catch((error) => { throw new Error(error) })
   }, [getAllTasks])
 
   const handleDeleteTask = (id) => {
@@ -28,7 +33,8 @@ export default function useHomePage () {
   const handleCreateTask = async (title) => {
     try {
       const data = await createTask({ title })
-      setTasks([...tasks, data])
+      const sortTasksFromApi = sortTasks([...tasks, data])
+      setTasks(sortTasksFromApi)
       setErrors({ ...errors, createTask: '' })
     } catch (error) {
       setErrors({ ...errors, createTask: error.message })
@@ -40,11 +46,12 @@ export default function useHomePage () {
     try {
       const data = await updateTask(updatedTask)
       const mappedTask = mapTaskFromApi(data)
-      console.log(mappedTask)
-      setTasks(tasks.filter((task) => {
-        if (task.id !== mappedTask.id) return mappedTask
+      const sortTasksFromApi = sortTasks(tasks.map((task) => {
+        if (task.id === mappedTask.id) return mappedTask
         return task
       }))
+
+      setTasks(sortTasksFromApi)
       setErrors({ ...errors, updateTask: '' })
     } catch (error) {
       setErrors({ ...errors, updateTask: error.message })
